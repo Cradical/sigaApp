e04f1fd430038d86eae6ae667e387646a06a5802
Conrad Wright <easp.conrad@gmail.com>
2018-11-01 13:31:03 -0600

    button added, basic function working.

--- screens/DeviceScreen.js
+++ screens/DeviceScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { AppRegistry, Text, StyleSheet, View, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 const sensorDataAPI = 'https://api.thingspeak.com/channels/615687/feeds.json?results=1'
 
 export default class DeviceScreen extends React.Component {
+
   static navigationOptions = {
     title: 'IoT Devices',
   };
@@ -11,7 +14,8 @@ export default class DeviceScreen extends React.Component {
   state = {
       channel: [],
       feeds: [],
-      test: 'test'
+      test: 'test',
+      isHidden: true
   }
 
   componentDidMount() {
@@ -22,35 +26,78 @@ export default class DeviceScreen extends React.Component {
           feeds: response.feeds
       }))
   }
+
+  onPressConsole() {
+      console.log('clicked')
+  }
   
   render() {
     console.log('feeds: ', this.state.feeds)
+    console.log(this.state.isHidden)
     return (
-        <View style={styles.container}>
-            <Text style={styles.contentContainer}>{this.state.channel.name}</Text>
+    <View style={styles.bodyContainer}>
+            <Text style={styles.basicContentStyling}>{this.state.channel.name}</Text>
             <Text style={styles.smallText}>Device ID: {this.state.channel.id}</Text>
-            <Text>{this.state.channel.field3} {this.state.feeds.map(item => {
-                return <Text key={item.entry_id}>{item.field3}</Text>
+            <Text style={{textAlign: 'center', textDecorationLine: 'underline'}}>Current Sensor Readings:</Text>
+        <View style={styles.mainContainer}>
+            <Text style={styles.sensorReadingStyling}>{this.state.channel.field3} {this.state.feeds.map((item, i) => {
+                return <Text style={{fontSize: 50, fontWeight: 'bold', textAlign: 'auto'}} key={i}>{item.field3}</Text>
             })}</Text>
-                {/* this code below will need to be refractored to account for unique keys */}
-            <Text>{this.state.channel.field1} {this.state.feeds.map(item => {
-                return <Text key={item.created_at}>{item.field1}</Text>
+            <Text style={styles.sensorReadingStyling}>{this.state.channel.field1} (%) {this.state.feeds.map((item, i) => {
+                return <Text style={{fontSize: 50, fontWeight: 'bold', textAlign: 'auto'}} key={i}>{item.field1}</Text>
             })}</Text>
         </View>
+        <Button
+            onPress={this.onPressConsole}
+            icon={
+                <Icon
+                name='arrow-right'
+                size={15}
+                color='white'
+                />
+            }
+            iconRight
+            title='BUTTON WITH RIGHT ICON'
+        />
+        {/* <View> */}
+        {/* {!this.state.isHidden ? <Text style={styles.basicContentStyling}>Toggled Box</Text> : null} */}
+        {/* </View> */}
+    </View>
         )
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
+    bodyContainer: {
         flex: 1,
         paddingTop: 15,
         backgroundColor: '#fff'
     },
-    contentContainer: {
+    mainContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        flexBasis: 100,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: '#aeaeae',
+        margin: 15,
+        padding: 2
+    },
+    basicContentStyling: {
         textAlign: 'center',
         fontSize: 20,
         fontWeight: 'bold'
+    },
+    sensorReadingStyling: {
+        display: 'flex',
+        flexDirection: 'column',
+        flexBasis: 100,
+        flex: 2,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        borderWidth: 1,
+        margin: 7
     },
     smallText: {
         textAlign: 'center',
