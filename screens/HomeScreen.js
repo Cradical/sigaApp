ff9b79934337c970ff1886d366cddf7afb7de8a7
Conrad Wright <easp.conrad@gmail.com>
2018-11-25 14:41:25 -0700

    Revert "refactor and code clean on HomeScreen."
    This reverts commit b3030371d2e2b27dbff1e35dd3910eb76be503a9.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {
   Image,
+  Platform,
   ScrollView,
   StyleSheet,
   Text,
-  View
+  TouchableOpacity,
+  View,
 } from 'react-native';
+import { WebBrowser } from 'expo';
+
 import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
@@ -15,9 +19,11 @@ export default class HomeScreen extends React.Component {
 
   state = {
     products: ['Coffee', 'Talapia'],
+    iot_devices: '#1560A',
     userProfile: {
       first_name: 'Conrad'
-    }
+    }, 
+    status: 'Active'
   }
 
   render() {
@@ -26,7 +32,11 @@ export default class HomeScreen extends React.Component {
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <View style={styles.welcomeContainer}>
           <Image
-              source={ require('../assets/images/atomix_user31.png') }
+              source={
+                __DEV__
+                  ? require('../assets/images/atomix_user31.png')
+                  : require('../assets/images/atomix_user31.png')
+              }
               style={styles.welcomeImage}
             />
             <View style={styles.contentContainer}>
@@ -47,12 +57,52 @@ export default class HomeScreen extends React.Component {
       </View>
     );
   }
+
+  _maybeRenderDevelopmentModeWarning() {
+    if (__DEV__) {
+      const learnMoreButton = (
+        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+          Learn more
+        </Text>
+      );
+
+      return (
+        <Text style={styles.developmentModeText}>
+          Development mode is enabled, your app will be slower but you can use useful development
+          tools. {learnMoreButton}
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.developmentModeText}>
+          You are not in development mode, your app will run at full speed.
+        </Text>
+      );
+    }
+  }
+
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+  };
+
+  _handleHelpPress = () => {
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+    );
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
   },
   contentContainer: {
     paddingTop: 30,
@@ -72,6 +122,71 @@ const styles = StyleSheet.create({
     shadowOpacity: 13,
     shadowColor: 'rgba(96,100,109, 0.8)',
     shadowOffset: {width: 12, height: 12} 
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+  userInfo: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    textAlign: 'center'
   },
   titleDisplay: {
     fontSize: 25,
